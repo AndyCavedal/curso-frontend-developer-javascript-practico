a1484f39793c08be875f229adc17ea53bb942ebe
Andy Cavedal <hcavedal@gmail.com>
2022-11-03 19:42:40 -0300

    Agregado Mobile-menu, y Media query para mobile menu y desktop menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const menuHamburguesa = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
+menuHamburguesa.addEventListener('click', toggleMobile);
 menuEmail.addEventListener('click', toggleDesktop);
 
 function toggleDesktop() {
     menuDesktop.classList.toggle('inactive');
+}
+
+function toggleMobile() {
+    menuMobile.classList.toggle('inactive');
 }
