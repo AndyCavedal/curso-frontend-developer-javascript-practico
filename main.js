e2e0be7fe6abe0c7304dcaea7f94e6b94d64ea45
Andy Cavedal <hcavedal@gmail.com>
2022-11-03 20:53:10 -0300

    Agregado aside de carrito optimizado para mobile y desktop

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const asideProductos = document.querySelector('.product-detail');
 
-menuHamburguesa.addEventListener('click', toggleMobile);
 menuEmail.addEventListener('click', toggleDesktop);
+menuHamburguesa.addEventListener('click', toggleMobile);
+shoppingCartIcon.addEventListener('click', toggleAsideProductDetail);
 
 function toggleDesktop() {
     menuDesktop.classList.toggle('inactive');
+    asideProductos.classList.add('inactive');
 }
 
 function toggleMobile() {
     menuMobile.classList.toggle('inactive');
+    asideProductos.classList.add('inactive');
+}
+
+function toggleAsideProductDetail() {
+    asideProductos.classList.toggle('inactive');
+    menuMobile.classList.add('inactive');
+    menuDesktop.classList.add('inactive');
 }
