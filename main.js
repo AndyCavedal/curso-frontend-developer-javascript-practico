d67350d535b9a5aed16b1d891af47f25bf2313a1
Andy Cavedal <hcavedal@gmail.com>
2022-11-07 18:34:11 -0300

    Agregado el asaid id: product detail. Tamnbien solucionado tamanio de mobile menu al desplegarse, ahora ocupa el 100% del width sin generar scroll

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuDesktop = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
 const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
-const asideProductos = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktop);
@@ -12,16 +12,16 @@ shoppingCartIcon.addEventListener('click', toggleAsideProductDetail);
 
 function toggleDesktop() {
     menuDesktop.classList.toggle('inactive');
-    asideProductos.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 
 function toggleMobile() {
     menuMobile.classList.toggle('inactive');
-    asideProductos.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
 
 function toggleAsideProductDetail() {
-    asideProductos.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
     menuMobile.classList.add('inactive');
     menuDesktop.classList.add('inactive');
 }
